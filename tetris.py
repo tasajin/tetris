d39a828a96dbbaa16d5a015514d7652327380347
tasajin <132569473+tasajin@users.noreply.github.com>
2023-06-08 21:48:00 +0900

    ミノが落ちていく
    画面外まで落ちる。新しいミノも出てこない

--- tetris.py
+++ tetris.py
@@ -23,7 +23,7 @@ masu_data = [[0 for i in range(yoko + 2)] for j in range(tate + 2)]
 for i in range(tate + 1):
     masu_data[i][0] = 9
     masu_data[i][yoko + 1] = 9
-    masu_data[tate + 1] = [9 for i in range(yoko + 2)]
+masu_data[tate + 1] = [9 for i in range(yoko + 2)]
 
 
 class Tetris():
@@ -31,9 +31,11 @@ class Tetris():
         self.root = root
         self.canvas = tk.Canvas(self.root, width=yoko * masu_size, height=tate * masu_size, bg="gray")
         self.canvas.pack()
-        self.current_block = None
+        self.block = None
         self.choiseMino()
         self.drawMino()
+
+        self.root.after(500, self.dropMino)
 
 
     #盤面を描画　20×10
@@ -45,49 +47,41 @@ class Tetris():
                 x2 = x1 + masu_size
                 y2 = y1 + masu_size
                 color = block_color[masu_data[row + 1][col + 1]]
-                self.canvas.create_rectangle(x1, y1, x2, y2,fill = color, outline="white")
-
+                self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="white")
 
     #落ちてくるミノの種類を決定
     def choiseMino(self):
         mino_data = [
-            # I
             [
                 [0, 0, 0, 0],
                 [1, 1, 1, 1],
                 [0, 0, 0, 0],
                 [0, 0, 0, 0]
             ],
-            # O
             [
                 [2, 2],
                 [2, 2]
             ],
-            # S
             [
                 [0, 3, 3],
                 [3, 3, 0],
                 [0, 0, 0]
             ],
-            # Z
             [
                 [4, 4, 0],
                 [0, 4, 4],
                 [0, 0, 0]
             ],
-            # J
             [
                 [5, 0, 0],
                 [5, 5, 5],
                 [0, 0, 0]
             ],
-            # L
             [
                 [0, 0, 6],
                 [6, 6, 6],
                 [0, 0, 0]
             ],
-            # T
             [
                 [0, 7, 0],
                 [7, 7, 7],
@@ -95,9 +89,9 @@ class Tetris():
             ]
         ]
         mino = random.choice(mino_data)
-        self.current_block = mino
+        self.block = mino
         
-    #二次元配列にそれぞれの数値を代入
+    #二次元配列にmoniの数値を代入
         for row in range(len(mino)):
             for col in range(len(mino[0])):
                 if mino[row][col] != 0:
@@ -107,8 +101,9 @@ class Tetris():
 
     #盤面の数値ごとに、ブロックの色を表示していく。または更新していく
     def drawMino(self):
-        self.canvas.delete("all") 
-        #
+        self.canvas.delete("all")
+        self.board()
+
         for row in range(tate):
             for col in range(yoko):
                 x1 = col * masu_size
@@ -118,21 +113,30 @@ class Tetris():
                 color = block_color[masu_data[row + 1][col + 1]]
                 self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="white")
 
-        self.root.after(500, self.dropMino)
-
-
-
-        
-            
+       
     #落ちる間隔を0.5秒で設定、⇩で同様に 
     def dropMino(self):
+        if not self.move(0, 1):
+            self.choiseMino()
+        self.drawMino()
+        
+        self.root.after(500, self.dropMino)
 
-        self.choiseMino()
+    def move(self, dx, dy):
+        new_block = [[0 for _ in range(yoko + 2)] for _ in range(tate + 2)]
+        for row in range(tate, 0, -1):
+            for col in range(1, yoko + 1):
+                if masu_data[row][col] != 0:
+                    if new_block[row + dy][col + dx] != 0 or new_block[row + dy][col + dx] == 9:
+                        return False
+                    new_block[row + dy][col + dx] = masu_data[row][col]
 
+        for row in range(1, tate + 2):
+            for col in range(1, yoko + 1):
+                masu_data[row][col] = new_block[row][col]
 
+        return True
 
-    
-    
     #下で停止
 
 
